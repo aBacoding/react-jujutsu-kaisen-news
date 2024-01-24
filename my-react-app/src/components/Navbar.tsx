db5ff3c3b3698b81b3606296a51acc0c59943bab
Abdurakhim Bakytzhan <97093590+aBacoding@users.noreply.github.com>
2024-01-24 10:44:22 +0600

    feat: Update navbar to ensure adaptability

--- my-react-app/src/components/Navbar.tsx
+++ my-react-app/src/components/Navbar.tsx
@@ -1,23 +1,45 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Navbar: React.FC = () => {
 	const [isAnimePopupOpen, setIsAnimePopupOpen] = useState(false)
 	const [isMangaPopupOpen, setIsMangaPopupOpen] = useState(false)
+	const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
 
 	const toggleAnimePopup = () => setIsAnimePopupOpen(!isAnimePopupOpen)
 	const toggleMangaPopup = () => setIsMangaPopupOpen(!isMangaPopupOpen)
+	const toggleMobileMenu = () => setIsMobileMenuOpen(!isMobileMenuOpen)
+
+	useEffect(() => {
+		const handleResize = () => {
+			if (window.innerWidth > 1024) {
+				setIsMobileMenuOpen(false)
+				setIsAnimePopupOpen(false)
+				setIsMangaPopupOpen(false)
+			}
+		}
+
+		window.addEventListener('resize', handleResize)
+
+		handleResize()
+
+		return () => window.removeEventListener('resize', handleResize)
+	}, [])
 
 	return (
-		<header className='navbar'>
+		<header
+			className={`navbar ${isMobileMenuOpen ? 'navbar--fullscreen' : ''}`}
+		>
 			<div className='container'>
-				<a
-					href='/'
-					className='navbar__logo'
-					onContextMenu={e => e.preventDefault()}
-				>
-					<img src='./images/logo.png' alt='Logo' draggable='false' />
-				</a>
-				<nav>
+				{!isMobileMenuOpen && (
+					<a
+						href='/'
+						className='navbar__logo'
+						onContextMenu={e => e.preventDefault()}
+					>
+						<img src='./images/logo.png' alt='Logo' draggable='false' />
+					</a>
+				)}
+				<nav className={`${isMobileMenuOpen ? 'mobile-nav' : ''}`}>
 					<ul className='navbar__links'>
 						<li>
 							<a href='/' className='nav-link'>
@@ -42,6 +64,16 @@ const Navbar: React.FC = () => {
 										<li>
 											<a href='/' className='popup-link'>
 												Characters
+											</a>
+										</li>
+										<li>
+											<a href='/' className='popup-link'>
+												Openings
+											</a>
+										</li>
+										<li>
+											<a href='/' className='popup-link'>
+												Endings
 											</a>
 										</li>
 									</ul>
@@ -89,6 +121,16 @@ const Navbar: React.FC = () => {
 						</li>
 					</ul>
 				</nav>
+				<div
+					className={`hamburger ${isMobileMenuOpen ? 'is-active' : ''}`}
+					onClick={toggleMobileMenu}
+				>
+					{isMobileMenuOpen ? (
+						<i className='fa fa-times'></i>
+					) : (
+						<i className='fa fa-bars'></i>
+					)}
+				</div>
 			</div>
 		</header>
 	)

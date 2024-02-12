57617081d0ff035586a73e6e2ba032fb1ee2d18e
Abdurakhim Bakytzhan <97093590+aBacoding@users.noreply.github.com>
2024-02-12 09:55:00 +0600

    feat: Revamp Home component with Jujutsu Kaisen theme

--- my-react-app/src/pages/Home.tsx
+++ my-react-app/src/pages/Home.tsx
@@ -9,9 +9,21 @@ const Home: React.FC<{ isMobileMenuOpen: boolean }> = ({
 
 	return (
 		<div className='container home-container'>
-			<div className='home'>
-				<h1>Welcome to the Homepage</h1>
-				<p>This is a basic homepage component.</p>
+			<div className='home__content'>
+				<h1>Jujutsu Kaisen</h1>
+				<p>
+					Yuji Itadori, a teenager who joins a secret organization of Jujutsu
+					Sorcerers to battle malevolent curses after ingesting a cursed object
+					and becoming host to Sukuna, a powerful curse. As he delves into a
+					world of supernatural threats and dark powers, Yuji must navigate
+					deadly battles and moral complexities. The series combines action,
+					horror, and fantasy, showcasing thrilling combat, deep character
+					development, and exploration of themes like courage and the essence of
+					evil.
+				</p>
+			</div>
+			<div className='image__container'>
+				<img src='./images/sukuna.png' className='sukuna__img' alt='Sukuna' />
 			</div>
 		</div>
 	)

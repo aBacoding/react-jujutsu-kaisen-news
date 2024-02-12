48f2caae96ffb1e90390fb87aa3f0c4ad8641b2d
Abdurakhim Bakytzhan <97093590+aBacoding@users.noreply.github.com>
2024-02-12 13:02:13 +0600

    feat: add Start the Journey button in Home component that redirect to News

--- my-react-app/src/pages/Home.tsx
+++ my-react-app/src/pages/Home.tsx
@@ -1,4 +1,7 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { faGlobe } from '@fortawesome/free-solid-svg-icons'
 
 const Home: React.FC<{ isMobileMenuOpen: boolean }> = ({
 	isMobileMenuOpen,
@@ -12,15 +15,24 @@ const Home: React.FC<{ isMobileMenuOpen: boolean }> = ({
 			<div className='home__content'>
 				<h1>Jujutsu Kaisen</h1>
 				<p>
-					Yuji Itadori, a teenager who joins a secret organization of Jujutsu
-					Sorcerers to battle malevolent curses after ingesting a cursed object
-					and becoming host to Sukuna, a powerful curse. As he delves into a
-					world of supernatural threats and dark powers, Yuji must navigate
-					deadly battles and moral complexities. The series combines action,
-					horror, and fantasy, showcasing thrilling combat, deep character
-					development, and exploration of themes like courage and the essence of
-					evil.
+					<strong style={{ color: '#FF1493', fontWeight: '900' }}>
+						Yuji Itadori
+					</strong>
+					, a teenager who joins a secret organization of Jujutsu Sorcerers to
+					battle malevolent curses after ingesting a cursed object and becoming
+					host to Sukuna, a powerful curse. As he delves into a world of
+					supernatural threats and dark powers, Yuji must navigate deadly
+					battles and moral complexities. The series combines action, horror,
+					and fantasy, showcasing thrilling combat, deep character development,
+					and exploration of themes like courage and the essence of evil.
 				</p>
+				<Link
+					to='/news'
+					className='home-content-btn'
+					onContextMenu={e => e.preventDefault()}
+				>
+					<FontAwesomeIcon icon={faGlobe} /> Start the Journey
+				</Link>
 			</div>
 			<div className='image__container'>
 				<img src='./images/sukuna.png' className='sukuna__img' alt='Sukuna' />
